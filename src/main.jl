201398cb95f7bbee3a7db78b3f8c99036e461d3c
Soeking <great.from.g@gmail.com>
2019-03-15 00:12:19 +0900

    output

--- src/main.jl
+++ src/main.jl
@@ -1,45 +1,78 @@
 symbol = ['+','-','*','/']
 output = ""
+correct = 10
 
 function setnum(numbers)
     for first_symbol in symbol
         for second_symbol in symbol
             for third_symbol in symbol
-
+                pattern1(numbers,first_symbol,second_symbol,third_symbol)
+                pattern2(numbers,first_symbol,second_symbol,third_symbol)
+                pattern3(numbers,first_symbol,second_symbol,third_symbol)
+                pattern4(numbers,first_symbol,second_symbol,third_symbol)
+                pattern5(numbers,first_symbol,second_symbol,third_symbol)
             end
         end
     end
 end
 
-#((a-b)-c)-d
-function pattern1()
+function calc(op::Char,x,y)
+    if op == '+'
+        return x + y
+    elseif op == '-'
+        return x - y
+    elseif op == '*'
+        return x * y
+    elseif op == '/'
+        return x / y
+    else
+        println("ERROR")
+        exit()
+    end
+end
 
+#((a-b)-c)-d
+function pattern1(nums,op1,op2,op3)
+    if calc(op3, calc(op2, calc(op1, nums[1], nums[2]), nums[3]), nums[4]) == correct
+        global output = "(($(nums[1])$(op1)$(nums[2]))$(op2)$(nums[3]))$(op3)$(nums[4])"
+        finish()
+    end
 end
 
 #(a-(b-c))-d
-function pattern2()
-
+function pattern2(nums,op1,op2,op3)
+    if calc(op3, calc(op1, calc(op2, nums[2], nums[3]), nums[1]), nums[4]) == correct
+        global output = "($(nums[1])$(op1)($(nums[2])$(op2)$(nums[3])))$(op3)$(nums[4])"
+        finish()
+    end
 end
 
-
 #a-((b-c)-d)
-function pattern3()
-
+function pattern3(nums,op1,op2,op3)
+    if calc(op1, calc(op3, calc(op2, nums[2], nums[3]), nums[4]), nums[1]) == correct
+        global output = "$(nums[1])$(op1)(($(nums[2])$(op2)$(nums[3]))$(op3)$(nums[4]))"
+        finish()
+    end
 end
 
 #a-(b-(c-d))
-function pattern4()
-
+function pattern4(nums,op1,op2,op3)
+    if calc(op1, calc(op2, calc(op3, nums[3], nums[4]), nums[2]), nums[1]) == correct
+       global output = "$(nums[1])$(op1)($(nums[2])$(op2)($(nums[3])$(op3)$(nums[4])))"
+       finish()
+    end
 end
-
 
 #(a-b)-(c-d)
-function pattern5()
-
+function pattern5(nums,op1,op2,op3)
+    if calc(op2, calc(op1, nums[1], nums[2]), calc(op3, nums[3], nums[4])) == correct
+       global output = "($(nums[1])$(op1)$(nums[2]))$(op2)($(nums[3])$(op3)$(nums[4]))"
+       finish()
+    end
 end
 
-function calc()
-
+function finish()
+    println(output)
 end
 
 function main()
